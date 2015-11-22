62960550756adc81bbb03103e814691871c58fe8
Amr Alaa <n1amr1@gmail.com>
2015-11-22 23:50:27 +0200

    rq04

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -37,6 +37,7 @@ class Assignment
       # accept offset and limit input parameters
       # use the TodoList Model class to find all TodoLists, ordered by `list_due_date` descending, with specified row offset and row limit
       # return a collection of TodoList instances that represent the specified rows
+      TodoList.offset(offset).limit(limit).all.order(list_due_date: :desc)
   end
 
   #
